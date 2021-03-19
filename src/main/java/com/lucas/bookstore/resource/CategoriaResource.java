c3e414216cb98c5933e3db0bf22c60d8389e3101
Lucas Lima <lluuccaassfm@gmail.com>
2021-03-18 22:18:17 -0300

    Cria 'findAll' para categorias utilizando padrão DTO

--- src/main/java/com/lucas/bookstore/resource/CategoriaResource.java
+++ src/main/java/com/lucas/bookstore/resource/CategoriaResource.java
@@ -1,13 +1,18 @@
 package com.lucas.bookstore.resource;
 
 import com.lucas.bookstore.domain.Categoria;
+import com.lucas.bookstore.dtos.CategoriaDTO;
 import com.lucas.bookstore.service.CategoriaService;
+import com.sun.source.util.TaskListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -20,5 +25,13 @@ public class CategoriaResource {
   public ResponseEntity<Categoria> findbyId(@PathVariable Long id){
     Categoria categoria = categoriaService.findById(id);
     return  ResponseEntity.ok().body(categoria);
+  }
+
+  @GetMapping
+  public ResponseEntity<List<CategoriaDTO>> findAll() {
+    List<Categoria> categorias = categoriaService.findAll();
+    List<CategoriaDTO> categoriasDTO = categorias.stream()
+            .map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+    return ResponseEntity.ok().body(categoriasDTO);
   }
 }
--- src/main/java/com/lucas/bookstore/service/CategoriaService.java
+++ src/main/java/com/lucas/bookstore/service/CategoriaService.java
@@ -1,11 +1,13 @@
 package com.lucas.bookstore.service;
 
+import com.lucas.bookstore.dtos.CategoriaDTO;
 import com.lucas.bookstore.exceptions.ObjectNotFoundException;
 import com.lucas.bookstore.domain.Categoria;
 import com.lucas.bookstore.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +19,9 @@ public class CategoriaService {
   public Categoria findById(Long id) {
     Optional<Categoria> obj = categoriaRepository.findById(id);
     return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: "+ Categoria.class.getName()));
+  }
+
+  public List<Categoria> findAll() {
+    return categoriaRepository.findAll();
   }
 }
